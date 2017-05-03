fe2f80224c16565e0e5c3d941af43ce99dff8fa1
Erich Wang <eriwan@microsoft.com>
2017-05-03 12:43:58 +0800

    workable DHT solution

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -9,15 +9,23 @@ using System.Threading.Tasks;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.Devices.Gpio;
+using Sensors.Dht;
 
 namespace Win10IoTCoreDiagnosticApp
 {
     public sealed partial class MainPage : Page
     {
-        private const string DeviceConnectionString = "HostName=erich-iothub1.azure-devices.net;DeviceId=d1;SharedAccessKey=sE2LiGxD9ekXNAs7tPZqRts5Jd0j00Qd6OKOBfeQnAg=";
+        private const string DeviceConnectionString = "HostName=erich-iothub1.azure-devices.net;DeviceId=d1;SharedAccessKey=ChraBvdqFY1fp2br6+sfGcOj9M2qcvOJhn/wCyHc62A=";
         private CancellationTokenSource _tokenSource;
         private readonly ContinuousDiagnosticProvider _diagnosticProvider;
         private readonly DeviceClientWrapper _deviceClient;
+        private DispatcherTimer _timer = new DispatcherTimer();
+        private Dht22 _dht;
+        private GpioPin _pin;
+        private long _continousErrorCount = 0;
+        private float _lastTemperature;
+        private float _lastHumidity;
+        private bool _hasLastValue = false;
 
         public MainPage()
         {
@@ -25,17 +33,84 @@ namespace Win10IoTCoreDiagnosticApp
             StartBtn.IsEnabled = true;
             StopBtn.IsEnabled = false;
             HelloMessage.Text = "Click button to send Diagnostic messages";
-            _diagnosticProvider = new ContinuousDiagnosticProvider(SamplingRateSource.Client, 100);
+
+            _diagnosticProvider = new ContinuousDiagnosticProvider(SamplingRateSource.Server, 10);
             _deviceClient = DeviceClientWrapper.CreateFromConnectionString(DeviceConnectionString, _diagnosticProvider);
+
+            _timer.Interval = TimeSpan.FromSeconds(2);
+            _timer.Tick += OnTimerTick;
+
+            HelloMessage.Text = "Start send D2C Message...";
+            StartBtn.IsEnabled = false;
+            StopBtn.IsEnabled = true;
+
+            _pin = GpioController.GetDefault().OpenPin(17, GpioSharingMode.Exclusive);
+            _dht = new Dht22(_pin, GpioPinDriveMode.Input);
+            _timer.Start();
         }
 
         private async void StartD2CMessageClick(object sender, RoutedEventArgs e)
         {
-            HelloMessage.Text = "Start send D2C Message...";
-            StartBtn.IsEnabled = false;
-            StopBtn.IsEnabled = true;
             //await SendD2CMessage();
-            DHT22.Start(HelloMessage);
+        }
+
+        private async void OnTimerTick(object sender, object e)
+        {
+            DhtReading reading = new DhtReading();
+
+            reading = await _dht.GetReadingAsync().AsTask();
+
+            var messageString = "";
+            if (reading.IsValid)
+            {
+                _lastTemperature = Convert.ToSingle(reading.Temperature);
+                _lastHumidity = Convert.ToSingle(reading.Humidity);
+                _hasLastValue = true;
+                _continousErrorCount = 0;
+            }
+            else
+            {
+                _continousErrorCount++;
+            }
+
+            bool isSensorOff = _continousErrorCount > 3;
+
+            if(!isSensorOff && _hasLastValue)
+            {
+                var telemetryDataPoint = new
+                {
+                    temperature = _lastTemperature,
+                    humidity = _lastHumidity
+                };
+                messageString = JsonConvert.SerializeObject(telemetryDataPoint);
+            }
+            else
+            {
+                var telemetryDataPoint = new
+                {
+                    humidity = ""
+                };
+                messageString = JsonConvert.SerializeObject(telemetryDataPoint);
+            }
+
+            if (isSensorOff || _hasLastValue)
+            {
+                while (true)
+                {
+                    try
+                    {
+                        var message = new Message(Encoding.ASCII.GetBytes(messageString));
+                        await _deviceClient.SendEventAsync(message);
+                        break;
+                    }
+                    catch (Exception ex)
+                    {
+                        Debug.WriteLine("Error occur: " + ex + "\n retry...");
+                        TipBox.Text = "Error occur: " + ex + "\n retry...";
+                        await Task.Delay(500);
+                    }
+                }
+            }
         }
 
         private void StopD2CMessageClick(object sender, RoutedEventArgs e)
@@ -43,7 +118,8 @@ namespace Win10IoTCoreDiagnosticApp
             HelloMessage.Text = "Click button to send Diagnostic messages";
             StopBtn.IsEnabled = false;
             StartBtn.IsEnabled = true;
-            _tokenSource.Cancel();
+            _timer.Stop();
+            //_tokenSource.Cancel();
         }
 
         private async Task SendD2CMessage()
